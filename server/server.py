ac3e188825f2418639078cf6df406f9cc01f3941
srikoushik <koushikrkmvcs@gmail.com>
2020-02-22 11:49:24 +0530

    Done minimal version of /getItemsbyColor

--- server/server.py
+++ server/server.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, Response
+import redis
 app = Flask(__name__)
+
+client = redis.Redis(host='redis', port=6379)
 
 @app.route("/")
 def main():
-  return "Flask server is running!!"
+	return "Flask server is running!!"
+
+@app.route("/getItemsbyColor/<color>")
+def get_items_by_color(color):
+	result = client.zrange(color, 0, 10)
+	return Response(result, mimetype='application/json')
 
 if __name__ == "__main__":
-  app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='0.0.0.0')
--- spark/app.py
+++ spark/app.py
@@ -7,7 +7,7 @@ import json
 import time
 import datetime
 
-client = redis.Redis(host='localhost', port=6379)
+client = redis.Redis(host='redis', port=6379)
 
 def update_to_redis(row):
     color_as_key(row)
