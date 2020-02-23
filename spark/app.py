08851e998be4fe9d46cb12a55e3e4b80480c3fc5
srikoushik <koushikrkmvcs@gmail.com>
2020-02-23 12:59:42 +0530

    Using dict and avoiding the redis call.

--- spark/app.py
+++ spark/app.py
@@ -7,11 +7,9 @@ import json
 import datetime
 
 client = redis.Redis(host='redis', port=6379)
+brand_count_for_date = dict()
 
 def update_to_redis(row):
-    color_and_date_as_key(row)
-
-def color_and_date_as_key(row):
     data_to_update = {
         "id": row['id'],
         "brand": row['brand'],
@@ -21,24 +19,28 @@ def color_and_date_as_key(row):
     product_created_date = convert_str_to_date_object(row['dateAdded'])
     date = product_created_date.strftime("%Y-%m-%d")
     score = product_created_date.timestamp()
+    
     update_product_count_for_date(row, date)
     client.zadd(row['colors'], {json.dumps(data_to_update) : int(score)})
     client.zadd(date, {json.dumps(data_to_update) : int(score)})
 
 def update_product_count_for_date(row, product_created_date):
-    value = client.get(product_created_date+row['brand'])
-    if(value == None):
-        client.set(product_created_date+row['brand'], 1)
-        client.zadd(product_created_date+'_count', {row['brand'] : 1})
-    else:
-        score = int(value) + 1
-        client.incr(product_created_date+row['brand'])
+    dict_key = product_created_date+row['brand']
+    try:
+        value = brand_count_for_date[dict_key]
+        score = value + 1
+        brand_count_for_date[dict_key] += 1
         client.zadd(product_created_date+'_count', {row['brand'] : score})
+    except KeyError:
+        brand_count_for_date[dict_key] = 1
+        client.zadd(product_created_date+'_count', {row['brand'] : 1})
 
 def convert_str_to_date_object(date_string):
     return datetime.datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
 
 if __name__ == '__main__':
+    before_time = time.time()
+
     scSpark = SparkSession \
         .builder \
         .appName("Reading csv") \
@@ -52,6 +54,9 @@ if __name__ == '__main__':
 
     data_without_null = transformed_data.where("id is not null and dateAdded is not null and colors is not null and brand is not null")
     
+    total_time = time.time() - before_time
+    print(f"Took {total_time} seconds before starting an update")
+
     start_time = time.time()
 
     pool = multiprocessing.Pool(processes = multiprocessing.cpu_count()-1)
